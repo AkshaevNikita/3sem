80248c5f9311f1a15d49871a8cca2015ec3dc1ad
AkshaevNikita <62900379+AkshaevNikita@users.noreply.github.com>
2020-09-25 11:56:39 +0300

    Add files via upload

---
+++ mycat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+ 
+void Cat(const int fd) {
+    char str[4096];
+    int n = read(fd, str, 4096), p = 0;
+    if (n < 0) {
+        perror("open failed");
+        exit(1);
+    }
+    while (n > 0) {
+        p = write(1, str, n);
+        if (p < 0) {
+            perror("open failed");
+            exit(1);
+        }
+        n -= p;
+    }
+}
+
+int main(int argc, char *argv[]){
+    if (argc == 1) {
+        while (1) 
+            Cat(0);
+    }
+
+    for (int i = 1; i < argc; i++) {
+        int fd = open(argv[i], O_RDWR | O_EXCL);    
+        Cat(fd);
+    }
+
+    return 0;
+} 
